db9db1513c1259eccbf3d8494707a10ff6f08336
AVS18 <avsadityavardhan18bcs@iiitkottayam.ac.in>
2021-12-18 10:03:27 +0530

    added urlpattern

--- ieeeBackend/urls.py
+++ ieeeBackend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import re_path
+from django.views.static import serve
 admin.site.site_header = 'IEEE LINK Web Login Panel'                    # default: "Django Administration"
 admin.site.index_title = 'Admin Arena'                 # default: "Site administration"
 admin.site.site_title = 'IEEE Kerala Section - LINK Team' # default: "Django site admin"
@@ -11,4 +13,8 @@ urlpatterns = [
     path('',include('base.urls')),
     path('',include('posts.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
